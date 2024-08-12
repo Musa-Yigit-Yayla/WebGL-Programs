bb4c338bf91ce9f8c4c5ca6ffc5a651c7ecbbb44
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2024-08-12 17:06:07 +0300

    Implementation elaboration initiated

--- windturbine/app.js
+++ windturbine/app.js
@@ -1,6 +1,57 @@
 import { mat4, vec3, vec4 } from 'gl-matrix';
 
-const CIRCLE_PRECISION = 200;
+let gl = document.getElementById('canvas').getContext('webgl2');
+const shaders = {
+    vs: 
+    `#version 300 es
+    
+    in vec3 vertices;
+    in vec3 vertColor;
+    out vec3 fragColor;
+
+    in vec2 rotation;
+
+    void main(){
+        gl_Position = vec4(vertices, 1.0);
+        fragColor = vertColor;
+    }
+
+    `,
+    fs: 
+    `#version 300 es
+
+    precision mediump float;
+    in vec3 fragColor;
+    out vec4 outColor;
+
+    void main(){
+        outColor = vec4(fragColor, 1.0);
+    }
+    `
+};
+let program;
+let vertexBuffer;
+let colorBuffer;
+
+const CIRCLE_PRECISION = 240;
+
+const initialPos = {
+    x: 0.0,
+    y: 0.0,
+    z: 0.8
+}
+
+const cameraPos = {
+    x: initialPos.x,
+    y: initialPos.y,
+    z: initialPos.z
+};
+
+// we look towards z- direction when both rotations are 0.0, if mouse goes up or right we increase rotation, else decrease it
+const rotation = {
+    rotX: 0.0,
+    rotY: 0.0
+}; 
 
 
 const mainPole = getCylinder(
@@ -58,41 +109,50 @@ function getCylinder(cx0, cy0, cz0, cx1, cy1, cz1, radius){
         circleVert1.push([currX, currY, distance]);
     }
     
+    const tarX = cx1 - cx0;
+    const tarY = cy1 - cy0;
+    const tarZ = cz1 - cz0; //careful here
+
     //now apply rotations to points
-    circleVert0 = rotateVertices(circleVert0);
-    circleVert1 = rotateVertices(circleVert1);
+    circleVert0 = rotateVertices(circleVert0, tarX, tarY, tarZ);
+    circleVert1 = rotateVertices(circleVert1, tarX, tarY, tarZ);
 
     return [circleVert0, circleVert1];
 }
 
-function renderFrame(gl){
+function renderFrame(){
 
     //render the main pole
     
-
+    renderCylinder(mainPole[0], mainPole[1], 0, 0, 0, [0.0, 0.0, 0.0]);
+    
+    requestAnimationFrame(renderFrame);
 }
 
-function renderCylinder(gl, circleVert0, circleVert1, rotX, rotY, rotZ , color){
+//color is expected to be an array with 3 entries, rgb respectively
+function renderCylinder( circleVert0, circleVert1, rotX, rotY, rotZ , color){
     
-    const shaders = {
-        vs: 
-        `#version 300 es
-        
-        in vec3 vertices;
+    let vertices0 = new Float32Array(circleVert0);
+    let vertices1 = new Float32Array(circleVert1);
+    
+    let wallVertices = new Float32Array(circleVert0.length * 3); //hold first two vertices from c0, then first vertex of c1, 
+    //then hold first two from c0 and second from c1, and then the second pair from c0 and second from c1, and then second pair from c0 and thrid from c1 and so on
 
-        void main(){
-            gl_Pos = vec4(vertices, 1.0);
+    let it = 0; //iterator for wallVertices
+    for(let i = 0; i < vertices0.length; i += 2){
+        for(let j = 0; j < 2; j++){
+            wallVertices[it++] = vertices0[i];
+            wallVertices[it++] = vertices0[i + 1];
+            wallVertices[it++] = vertices1[j];
         }
-
-        `,
-        fs: ``
-    };
+    }
+    
 }
 
 /**
  * 
  * @param {*} vert 2d array of 3D vertices
- * @param {*} tarX x coordinate for the tip of the given vector (the tip is origin) 
+ * @param {*} tarX x coordinate for the tip of the given vector (the other tip is origin) 
  * @return rotated vertex positions
  */
 function rotateVertices(vert, tarX, tarY, tarZ){
@@ -125,7 +185,26 @@ function rotateVertices(vert, tarX, tarY, tarZ){
     return rotatedVert;
 }
 
+function initProgram(){
+    program = gl.createProgram();
+    
+    let vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    let fragShader = gl.createShader(gl.FRAGMENT_SHADER);
 
+    gl.shaderSource(vertexShader, shaders.vs);
+    gl.shaderSource(fragShader, shaders.fs);
+
+    gl.compileShader(vertexShader);
+    gl.compileShader(fragShader);
+
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragShader);
+
+    vertexBuffer = gl.createBuffer();
+    colorBuffer = gl.createBuffer();
+}
+
+initProgram();
 
 
 
