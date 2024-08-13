9c0aed41fb5b03de8eef240c6874c2ad9bec4f71
Musa Yiğit Yayla <yigit.yayla@ug.bilkent.edu.tr>
2024-08-13 13:18:46 +0300

    Further adjustments

--- windturbine/app.js
+++ windturbine/app.js
@@ -1,4 +1,6 @@
-import { mat4, vec3, vec4 } from 'gl-matrix';
+console.log(math);
+
+
 
 let gl = document.getElementById('canvas').getContext('webgl2');
 const shaders = {
@@ -7,6 +9,7 @@ const shaders = {
     
     in vec3 vertices;
     in vec3 vertColor;
+    uniform vec3 cameraPos;
     out vec3 fragColor;
 
     in vec2 rotation;
@@ -121,6 +124,7 @@ function getCylinder(cx0, cy0, cz0, cx1, cy1, cz1, radius){
 }
 
 function renderFrame(){
+    console.log("Debug: renderFrame invoked");
 
     //render the main pole
     
@@ -131,9 +135,24 @@ function renderFrame(){
 
 //color is expected to be an array with 3 entries, rgb respectively
 function renderCylinder( circleVert0, circleVert1, rotX, rotY, rotZ , color){
-    
+    console.log("Debug: renderCylinder invoked");
+
+
     let vertices0 = new Float32Array(circleVert0);
     let vertices1 = new Float32Array(circleVert1);
+    
+    let vertices = new Float32Array(vertices0.length + vertices1.length);
+    for(let i = 0; i < vertices.length; i++){
+        if(i < vertices0.length){
+            vertices[i] = vertices0[i];
+        }
+        else{
+            vertices[i] = vertices1[i - vertices0.length];
+        }
+    }
+
+    let colorArr = new Float32Array(color);
+    let cameraArr = new Float32Array([cameraPos.x, cameraPos.y, cameraPos.z]);
     
     let wallVertices = new Float32Array(circleVert0.length * 3); //hold first two vertices from c0, then first vertex of c1, 
     //then hold first two from c0 and second from c1, and then the second pair from c0 and second from c1, and then second pair from c0 and thrid from c1 and so on
@@ -147,6 +166,20 @@ function renderCylinder( circleVert0, circleVert1, rotX, rotY, rotZ , color){
         }
     }
     
+    let verticesLoc = gl.getAttribLocation(program, 'vertices');
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+    
+    gl.vertexAttribPointer(verticesLoc, 3, gl.FLOAT, gl.FALSE, 0, 0);
+
+    let vertColorLoc = gl.getAttribLocation(program, 'vertColor');
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, colorArr, gl.STATIC_DRAW);
+    gl.vertexAttribPointer(vertColorLoc, 3, gl.FLOAT, gl.FALSE, 0, 0);
+
+    gl.drawArrays(gl.TRIANGLE_FAN, 0, vertices.length / 3);
 }
 
 /**
@@ -155,37 +188,45 @@ function renderCylinder( circleVert0, circleVert1, rotX, rotY, rotZ , color){
  * @param {*} tarX x coordinate for the tip of the given vector (the other tip is origin) 
  * @return rotated vertex positions
  */
-function rotateVertices(vert, tarX, tarY, tarZ){
+function rotateVertices(vert, tarX, tarY, tarZ) {
     let rotatedVert = [];
 
-    const currAxis = vec3.fromValues(0, 0, 1);
-    const targetVector = vec3.fromValues(tarX, tarY, tarZ);
-    
-    //normalize the target vector
-    vec3.normalize(targetVector, targetVector);
+    // Define current axis (Z-axis)
+    let currAxis = math.matrix([0, 0, 1]);
+
+    // Define target vector
+    let targetVector = math.matrix([tarX, tarY, tarZ]);
+
+    // Normalize the target vector manually
+    let magnitude = math.norm(targetVector);
+    targetVector = math.dotDivide(targetVector, magnitude);
 
     // Compute the rotation axis
-    const rotationAxis = vec3.create();
-    vec3.cross(rotationAxis, currAxis, targetVector);
+    let rotationAxis = math.cross(currAxis, targetVector);
 
     // Compute the rotation angle
-    const cosTheta = vec3.dot(currentAxis, targetVector);
-    const angle = Math.acos(cosTheta);
+    let cosTheta = math.dot(currAxis, targetVector);
+    let angle = Math.acos(cosTheta);
 
-    // Create the rotation matrix
-    const rotationMatrix = mat4.create();
-    mat4.fromRotation(rotationMatrix, angle, rotationAxis);
+    // Create the rotation matrix for rotation around the Z-axis
+    let rotationMatrix = math.matrix([
+        [Math.cos(angle), -Math.sin(angle), 0],
+        [Math.sin(angle), Math.cos(angle), 0],
+        [0, 0, 1]
+    ]);
 
-    for(let i = 0; i < vert.length; i++){
-        let vertex = vec4.fromValues(vert[i][0], vert[i][1], vert[i][2], 1);
-        const rotatedVec = vec4.create();
-        vec4.transformMat4(rotatedVec, vertex, rotationMatrix);
+    // Rotate each vertex
+    for (let i = 0; i < vert.length; i++) {
+        let vertex = math.matrix([vert[i][0], vert[i][1], vert[i][2]]);
+        let rotatedVec = math.multiply(rotationMatrix, vertex).toArray();
         rotatedVert.push([rotatedVec[0], rotatedVec[1], rotatedVec[2]]);
     }
+
     return rotatedVert;
 }
-
 function initProgram(){
+    console.log("Debug: initProgram invoked");
+    
     program = gl.createProgram();
     
     let vertexShader = gl.createShader(gl.VERTEX_SHADER);
@@ -199,12 +240,14 @@ function initProgram(){
 
     gl.attachShader(program, vertexShader);
     gl.attachShader(program, fragShader);
+    gl.linkProgram(program);
+    gl.useProgram(program);
 
     vertexBuffer = gl.createBuffer();
     colorBuffer = gl.createBuffer();
 }
 
 initProgram();
-
+renderFrame();
 
 
